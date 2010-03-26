4ae2e4b1d9de366d6201961c674215d62df6caa7
Karsten Loesing <karsten.loesing@gmx.net>
2010-03-26 14:21:06 +0100

    Start JVM with 1GB memory, or some processing steps will fail.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp bin/:lib/commons-codec-1.4.jar:lib/commons-compress-1.0.jar -Djava.util.logging.config.file=logging.properties Main
+java -Xms128m -Xmx1024m -cp bin/:lib/commons-codec-1.4.jar:lib/commons-compress-1.0.jar -Djava.util.logging.config.file=logging.properties Main
 
