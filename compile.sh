1891e9d5978237ac23ef7563f1873d29cfeb83d5
Karsten Loesing <karsten.loesing@gmx.net>
2010-04-13 19:25:07 +0200

    Replace shell scripts with Ant build file.

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-javac -d bin/ -cp src:lib/commons-codec-1.4.jar:lib/commons-compress-1.0.jar src/*.java
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-java -Xms128m -Xmx1024m -cp bin/:lib/commons-codec-1.4.jar:lib/commons-compress-1.0.jar:lib/postgresql-8.4-701.jdbc4.jar Main
+# TODO is there a better way to suppress Ant's output?
+ant -q | grep -Ev "^$|^BUILD SUCCESSFUL|^Total time: "
 
