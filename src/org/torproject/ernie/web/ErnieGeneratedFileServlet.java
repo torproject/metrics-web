314caf945772db84d65bcbd3d92ee0ec9fe75763
Karsten Loesing <karsten.loesing@gmx.net>
2010-05-19 14:59:57 +0200

    Add servlet to return static content generated by ERNIE.

---
+++ src/org/torproject/ernie/web/ErnieGeneratedFileServlet.java
@@ -0,0 +1,49 @@
+package org.torproject.ernie.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.*;
+import java.util.*;
+
+public class ErnieGeneratedFileServlet extends HttpServlet {
+
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response) throws IOException,
+      ServletException {
+
+    /* Read file from disk and write it to response. */
+    String fn = "/srv/metrics.torproject.org/ernie/website"
+        + request.getRequestURI();
+    BufferedInputStream input = null;
+    BufferedOutputStream output = null;
+    try {
+      File f = new File(fn);
+      if (!f.exists()) {
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        return;
+      }
+      response.setContentType(this.getServletContext().getMimeType(f.getName()));
+      response.setHeader("Content-Length", String.valueOf(
+          f.length()));
+      response.setHeader("Content-Disposition",
+          "inline; filename=\"" + f.getName() + "\"");
+      input = new BufferedInputStream(new FileInputStream(f),
+          1024);
+      output = new BufferedOutputStream(response.getOutputStream(), 1024);
+      byte[] buffer = new byte[1024];
+      int length;
+      while ((length = input.read(buffer)) > 0) {
+          output.write(buffer, 0, length);
+      }
+    } finally {
+      if (output != null) {
+        output.close();
+      }
+      if (input != null) {
+        input.close();
+      }
+    }
+  }
+}
+
