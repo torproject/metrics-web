a1ecb962beffa936d50a5f63d54bb84f0ebb7e72
Karsten Loesing <karsten.loesing@gmx.net>
2011-01-31 14:29:36 +0100

    Add --no-save switch to Rserve start script.
    R requires the --no-save switch when run non-interactively.  This is why
    Rserve didn't survive a server reboot so far.  Now it should.

--- rserve/start.sh
+++ rserve/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 dir=`pwd`
-R CMD /home/metrics/R/x86_64-pc-linux-gnu-library/2.11/Rserve/libs/Rserve-bin.so --RS-conf $dir/Rserv.conf >> rserve.log 2>&1
+R CMD /home/metrics/R/x86_64-pc-linux-gnu-library/2.11/Rserve/libs/Rserve-bin.so --no-save --RS-conf $dir/Rserv.conf >> rserve.log 2>&1
