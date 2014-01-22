28b2e308dc03ad349e70d44f43409b1c2d2f4ec0
Karsten Loesing <karsten.loesing@gmx.net>
2014-01-22 13:43:03 +0100

    Create stats directory if it doesn't exist.

--- shared/bin/50-run-legacy-stats.sh
+++ shared/bin/50-run-legacy-stats.sh
@@ -2,6 +2,7 @@
 cd modules/legacy/
 ant | grep "\[java\]"
 psql -U metrics tordir -c 'SELECT * FROM refresh_all();'
+mkdir -p stats
 psql -c 'COPY (SELECT * FROM stats_servers) TO STDOUT WITH CSV HEADER;' tordir > stats/servers.csv
 psql -c 'COPY (SELECT * FROM stats_bandwidth) TO STDOUT WITH CSV HEADER;' tordir > stats/bandwidth.csv
 psql -c 'COPY (SELECT * FROM stats_torperf) TO STDOUT WITH CSV HEADER;' tordir > stats/torperf.csv
