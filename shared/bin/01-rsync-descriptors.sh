d1b7eb152982c2de2bebf7caf0dc5195fa183a9e
Karsten Loesing <karsten.loesing@gmx.net>
2014-01-22 09:20:45 +0100

    Exclude microdescriptors from rsync.

--- shared/bin/01-rsync-descriptors.sh
+++ shared/bin/01-rsync-descriptors.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rsync -arz --delete --exclude 'relay-descriptors/votes' metrics.torproject.org::metrics-recent shared/in
+rsync -arz --delete --exclude 'relay-descriptors/votes' --exclude 'relay-descriptors/microdescs' metrics.torproject.org::metrics-recent shared/in
 
