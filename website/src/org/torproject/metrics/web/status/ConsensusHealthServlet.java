890b6c9624edf46d31f742ab42ec5f838f3e03eb
Karsten Loesing <karsten.loesing@gmx.net>
2014-01-22 13:06:47 +0100

    Simplify note that consensus-health has moved.

--- website/src/org/torproject/metrics/web/status/ConsensusHealthServlet.java
+++ website/src/org/torproject/metrics/web/status/ConsensusHealthServlet.java
@@ -1,11 +1,7 @@
-/* Copyright 2011, 2012 The Tor Project
+/* Copyright 2011--2014 The Tor Project
  * See LICENSE for licensing information */
 package org.torproject.metrics.web.status;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -15,43 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ConsensusHealthServlet extends HttpServlet {
 
-  private static final long serialVersionUID = -5230032733057814869L;
+  private static final long serialVersionUID = 8349991221914797433L;
 
   public void doGet(HttpServletRequest request,
-      HttpServletResponse response) throws IOException,
-      ServletException {
+      HttpServletResponse response) throws IOException, ServletException {
 
-    /* Read file from disk and write it to response. */
-    BufferedInputStream input = null;
-    BufferedOutputStream output = null;
-    try {
-      File f = new File("/srv/metrics.torproject.org/ernie/website/"
-          + "consensus-health.html");
-      if (!f.exists()) {
-        response.sendError(HttpServletResponse.SC_NOT_FOUND);
-        return;
-      }
-      response.setContentType(this.getServletContext().getMimeType(f.getName()));
-      response.setHeader("Content-Length", String.valueOf(
-          f.length()));
-      response.setHeader("Content-Disposition",
-          "inline; filename=\"" + f.getName() + "\"");
-      input = new BufferedInputStream(new FileInputStream(f),
-          1024);
-      output = new BufferedOutputStream(response.getOutputStream(), 1024);
-      byte[] buffer = new byte[1024];
-      int length;
-      while ((length = input.read(buffer)) > 0) {
-          output.write(buffer, 0, length);
-      }
-    } finally {
-      if (output != null) {
-        output.close();
-      }
-      if (input != null) {
-        input.close();
-      }
-    }
+    /* Forward the request to the JSP that does all the hard work. */
+    request.getRequestDispatcher("WEB-INF/consensus-health.jsp").forward(
+        request, response);
   }
 }
 
