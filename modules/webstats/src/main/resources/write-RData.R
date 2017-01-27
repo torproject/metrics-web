d755f4113d7f201e60c9fc4dc70f49bad614da73
Karsten Loesing <karsten.loesing@gmx.net>
2017-01-27 19:54:16 +0100

    Add webstats-tm graph very similar to webstats-tb.

--- modules/webstats/src/main/resources/write-RData.R
+++ modules/webstats/src/main/resources/write-RData.R
@@ -7,3 +7,10 @@ data <- aggregate(list(count = d$count),
     FUN = sum)
 save(data, file = "RData/webstats-tb.RData")
 
+d <- read.csv("stats/webstats.csv", stringsAsFactors = FALSE)
+d <- d[d$request_type %in% c('tmid', 'tmup'), ]
+data <- aggregate(list(count = d$count),
+    by = list(log_date = as.Date(d$log_date), request_type = d$request_type),
+    FUN = sum)
+save(data, file = "RData/webstats-tm.RData")
+
