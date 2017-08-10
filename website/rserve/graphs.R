5a15bead74d53173308eccd9ff16b54267fd4264
Karsten Loesing <karsten.loesing@gmx.net>
2017-08-10 16:23:56 +0200

    Add 0.3.1 and 0.3.2 to known versions.
    Fixes #23165.

--- website/rserve/graphs.R
+++ website/rserve/graphs.R
@@ -356,7 +356,7 @@ plot_versions <- function(start, end, path) {
                   relays = s$relays)
   known_versions <- c("Other", "0.1.0", "0.1.1", "0.1.2", "0.2.0",
         "0.2.1", "0.2.2", "0.2.3", "0.2.4", "0.2.5", "0.2.6", "0.2.7",
-        "0.2.8", "0.2.9", "0.3.0")
+        "0.2.8", "0.2.9", "0.3.0", "0.3.1", "0.3.2")
   getPalette = colorRampPalette(brewer.pal(12, "Paired"))
   colours <- data.frame(breaks = known_versions,
     values = rep(brewer.pal(min(12, length(known_versions)), "Paired"),
