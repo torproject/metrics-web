9554c558608741ec0bc400744b8d25b82a46f275
iwakeh <iwakeh@torproject.org>
2017-12-04 11:14:27 +0000

    Add ant task for legacy run.
    Move related sources to main src tree,
    delete old build.xml and shell script.
    Update .gitignore to ignore legacy.config.
    The psql executions can contain all command line parameters of psql, i.e.,
    it is also possible to add username and more.  Currently, only the db name is
    set.  This situation could be made easier/more consistent, but this is a new task
    not part of the current.

--- shared/bin/50-run-legacy-stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd modules/legacy/
-ant | grep "\[java\]"
-psql -U metrics tordir -c 'SELECT * FROM refresh_all();'
-mkdir -p stats
-psql -c 'COPY (SELECT * FROM stats_servers) TO STDOUT WITH CSV HEADER;' tordir > stats/servers.csv
-psql -c 'COPY (SELECT * FROM stats_bandwidth) TO STDOUT WITH CSV HEADER;' tordir > stats/bandwidth.csv
-cd ../../
-
