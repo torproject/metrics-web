338be5c1d24b4739c7fafa9870dc63bdda0ffe84
iwakeh <iwakeh@torproject.org>
2017-12-04 11:14:43 +0000

    Move web content and files to main src folder.

--- website/src/main/resources/bootstrap-development.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Copyright 2017 The Tor Project
-# See LICENSE for licensing information
-#
-# Use for submodule initialization and checkout.
-# Only necessary after very first checkout without recursion.
-#
-git submodule update --init --remote
