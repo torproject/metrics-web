e9f3226f5e9da0ecab358f85dbe418c0066bfb11
iwakeh <iwakeh@torproject.org>
2017-12-20 13:06:48 +0000

    Prefix module name to system property names.

--- src/main/java/org/torproject/metrics/stats/ipv6servers/Configuration.java
+++ src/main/java/org/torproject/metrics/stats/ipv6servers/Configuration.java
@@ -6,13 +6,13 @@ package org.torproject.metrics.stats.ipv6servers;
 /** Configuration options parsed from Java properties with reasonable hard-coded
  * defaults. */
 class Configuration {
-  static String descriptors = System.getProperty("descriptors",
+  static String descriptors = System.getProperty("ipv6servers.descriptors",
       "../../shared/in/");
-  static String database = System.getProperty("database",
+  static String database = System.getProperty("ipv6servers.database",
       "jdbc:postgresql:ipv6servers");
-  static String history = System.getProperty("history",
+  static String history = System.getProperty("ipv6servers.history",
       "status/read-descriptors");
-  static String output = System.getProperty("output",
+  static String output = System.getProperty("ipv6servers.output",
       "stats/ipv6servers.csv");
 }
 
